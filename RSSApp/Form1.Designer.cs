b82a8c9d8423fb631df0cae42bcb0f8564eecf0e
Davbjorn <david.timgren@gmail.com>
2018-10-23 12:23:12 +0200

    Added ListView for feed and category.

--- RSSApp/Form1.Designer.cs
+++ RSSApp/Form1.Designer.cs
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lvKategorier = new System.Windows.Forms.ListView();
+            this.lvFeed = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Podcasts";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(480, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Kategorier";
+            // 
+            // lvKategorier
+            // 
+            this.lvKategorier.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvKategorier.Location = new System.Drawing.Point(484, 37);
+            this.lvKategorier.Name = "lvKategorier";
+            this.lvKategorier.Size = new System.Drawing.Size(304, 210);
+            this.lvKategorier.TabIndex = 5;
+            this.lvKategorier.UseCompatibleStateImageBehavior = false;
+            // 
+            // lvFeed
+            // 
+            this.lvFeed.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvFeed.Location = new System.Drawing.Point(13, 38);
+            this.lvFeed.Name = "lvFeed";
+            this.lvFeed.Size = new System.Drawing.Size(465, 209);
+            this.lvFeed.TabIndex = 6;
+            this.lvFeed.UseCompatibleStateImageBehavior = false;
+            this.lvFeed.View = System.Windows.Forms.View.Details;
+            this.lvFeed.SelectedIndexChanged += new System.EventHandler(this.lvFeed_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Namn";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Avsnitt";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Kategori";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader3.Width = 124;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Namn";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Avsnitt";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Kategori";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvFeed);
+            this.Controls.Add(this.lvKategorier);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView lvKategorier;
+        private System.Windows.Forms.ListView lvFeed;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
     }
 }
 
--- RSSApp/Form1.cs
+++ RSSApp/Form1.cs
@@ -16,5 +16,20 @@ namespace RSSApp
         {
             InitializeComponent();
         }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void lvFeed_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
